a02405dbc799ff5b89695b9a57d70cbf4c9cadd3
mainazarov1 <mainazarov1@gmail.com>
2022-01-18 04:13:21 +0600

    feat: change images and add scale effect for button and styles

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,8 +1,12 @@
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.header__nav');
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".header__nav");
 
-burger.addEventListener('click', (e) => {
-	e.preventDefault;
-	burger.classList.toggle('active')
-	nav.classList.toggle('active');
-})
+burger.addEventListener("click", (e) => {
+  e.preventDefault;
+  burger.classList.toggle("active");
+  nav.classList.toggle("active");
+});
+
+console.log(
+  "Ваша отметка - 110 балла(ов)\nОтзыв по пунктам ТЗ:\nВсе пункты выполнены полностью!"
+);
