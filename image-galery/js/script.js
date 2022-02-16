c7f021819d2c9294f7de82c43dd7b583050d947e
mainazarov1 <mainazarov1@gmail.com>
2022-02-16 10:12:51 +0600

    feat: add styles to input and icons

--- image-galery/js/script.js
+++ image-galery/js/script.js
@@ -1,18 +1,38 @@
 const auth = 'AVa_hZA3gk37wO5xko9pKP3gkFLTk4FeapoThCMqFoc';
 
-const input = document.querySelector('#header__input');
-const search = document.querySelector('#header__search');
+const input = document.getElementById('header__input');
+const search = document.getElementById('header__search');
+const deleteValue = document.getElementById('header__delete-value');
 const gallery = document.querySelector('.gallery__grid');
 let query = '';
 getData('all');
+if (!query) {
+	input.addEventListener('input', (e) => {
+		e.target.value;
+		deleteValue.style.display = 'block';
+		deleteValue.addEventListener('click', (event) => {
+			event.preventDefault();
+			input.value = '';
+			deleteValue.style.display = 'none'
+		})
+	})
+}
 input.addEventListener('keypress', (e) => {
 	if (e.key == 'Enter') {
 		e.preventDefault();
+		query = e.target.value;
 		gallery.innerHTML = '';
-		query = e.target.value
 		getData(query);
 		getImages();
 	}
+	
+	// if (query !== '') {
+	// 	const deleteValueInput = document.createElement('img');
+	// 	deleteValueInput.classList.add('header__delete-value');
+	// 	deleteValueInput.src = '../assets/icons/x-circle.svg'
+	// 	const searchDiv = document.querySelector('.header__search');
+	// 	searchDiv.appendChild(deleteValueInput)
+	// }
 })
 
 search.addEventListener('click', (e) => {
